115da758d1950fcf8c0b129cb06db20e7700dde8
Prashant Patil <prashantpatilppg36@gmail.com>
2022-12-20 19:09:03 +0530

    first message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Question1Component } from './question1/question1.component';
+import { Question3Component } from './question3/question3.component';
+import { Question4Component } from './question4/question4.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'q1', component:Question1Component},
+  {path:'q3', component:Question3Component},
+  {path:'q4', component:Question4Component},
+  
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Question1Component } from './question1/question1.component';
+import { Question3Component } from './question3/question3.component';
+import { Question4Component } from './question4/question4.component';
+import { C1Component } from './question4/c1/c1.component';
+import { C2Component } from './question4/c2/c2.component';
+import { C3Component } from './question4/c3/c3.component';
+import { C4Component } from './question4/c4/c4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Question1Component,
+    Question3Component,
+    Question4Component,
+    C1Component,
+    C2Component,
+    C3Component,
+    C4Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
